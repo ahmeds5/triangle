287dab76a905d4954fc881a7f9c3cd801b93d51c
Lawrence Eby and said ahmed <said@>
2017-01-12 13:30:57 -0800

    set up jQuery ready.function, as well as store some variables.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#sides").submit(function(event){
+    var side1Input = parseInt($("#side1").val());
+    var side2Input = parseInt($("#side2").val());
+    var side3Input = parseInt($("#side3").val());
+
+
+  });
+
+
+
+});
